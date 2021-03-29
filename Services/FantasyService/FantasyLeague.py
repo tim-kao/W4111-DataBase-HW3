fb576e30109f972ea208ed068bda751dfa24c0c7
tim-kao <79119646+tim-kao@users.noreply.github.com>
2021-03-29 17:11:23 +0800

    Add files via upload

---
+++ Services/FantasyService/FantasyLeague.py
@@ -0,0 +1,13 @@
+from Services.BaseResource import ResourceBase
+
+
+class FantasyLeagueTO():
+
+    def __init__(self):
+        pass
+
+
+class FantasyLeague(ResourceBase):
+
+    def __init__(self, config_info):
+        super().__init__(config_info)
---
+++ Services/FantasyService/FantasyPlayer.py
@@ -0,0 +1,13 @@
+from Services.BaseResource import ResourceBase
+
+
+class FantasyPlayerTO():
+
+    def __init__(self):
+        pass
+
+
+class FantasyPlayer(ResourceBase):
+
+    def __init__(self, config_info):
+        super().__init__(config_info)
